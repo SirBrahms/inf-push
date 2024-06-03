abee30d59ab82fde5cc3edbc3c2084d523f6cd02
mjm42 <michael.morrison@edu.ksmenzingen.ch>
2024-06-03 22:04:13 +0200

    Logo
    -Added data folder for images and stuff
    -Logo screen on startup
    -Pygame initialisation, screen setup etc.
    
    (slightly janky)

---
+++ main.py
@@ -0,0 +1,47 @@
+import pygame
+pygame.init()
+
+#screen setup
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Push")
+#useful variables 
+screen_center_X = screen_width // 2
+screen_center_Y = screen_height // 2
+
+#logo
+logo_done = False
+logo = pygame.image.load("data/logo.png")
+logo_width, logo_height = logo.get_size()
+logo_X = screen_center_X - logo_width // 2
+logo_Y = screen_center_Y - logo_height // 2
+logo_display_time = 5000 #milliseconds
+logo_timer = pygame.time.get_ticks() 
+
+
+
+running = True
+while running:
+    #quit event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+            
+    #clear screen if logo displayed for long enough
+    current_time = pygame.time.get_ticks()
+    if current_time - logo_timer >= logo_display_time:
+        screen.fill((0, 0, 0))  
+        pygame.display.flip()   
+        pygame.time.delay(1000) 
+        running = False
+    
+    #or display logo
+    else:
+        screen.blit(logo, (logo_X, logo_Y))
+        pygame.display.flip()
+        
+#quitting
+pygame.time.wait(4000)
+pygame.quit()

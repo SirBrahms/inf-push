1c8b88cb3eb584e7a6550ef9044bac4002f09280
56356 <99555686+56356@users.noreply.github.com>
2024-06-04 09:43:19 +0200

    Update Player.py

---
+++ Player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self,name,avatar):
+        self.name=name
+        self.avatar=avatar
+        self.score=0
+        self.cards=[]
+
+    def get_card(self):
+        pass
+    def secure_color(self):
+        pass

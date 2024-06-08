f20575b8fa4ebf1b2f6e5d850efaf068444b38f6
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2024-06-08 14:15:05 +0200

    rework Colourcard class

--- Card.py
+++ Card.py
@@ -1,10 +1,10 @@
 class Card():
-    def __init__(self, Type):
-        self.Type = None
+    def __init__(self, path):
+        self.path = None
         
 class Colourcard(Card):
-    def __init__(self, Type, colour, number):
-        super().__init__(Type)
+    def __init__(self, path, colour, number):
+        super().__init__(path)
         self.colour = colour
         self.number = number
         

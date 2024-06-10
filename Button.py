47ec1ba8beca1f3e16583c5134850a19c0447a5c
mjm42 <michael.morrison@edu.ksmenzingen.ch>
2024-06-10 14:14:00 +0200

    added button scaling

--- Button.py
+++ Button.py
@@ -6,10 +6,18 @@ class Button:
         self.y = y
         self.scale = scale
         self.image_path = image_path
-    
+        self.width = None
+        self.height = None
+        self.rect = None
+        self.image = None
     
     def load_image(self):
         self.image = pygame.image.load(self.image_path)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (int(self.width * self.scale), int(self.height * self.scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
         
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))

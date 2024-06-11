95f4d99a871f3a4f1344c6efe8f1ee5d3e0687a5
SirBrahms <marc@famile-merz.ch>
2024-06-11 16:56:35 +0200

    Fix Cards and introduce type safety

--- Card.py
+++ Card.py
@@ -1,22 +1,22 @@
 import random
  
 class Card():
-    def __init__(self, path):
-        self.path = None
+    def __init__(self, path:str):
+        self.path = path
  
 class ColourCard(Card):
-    def __init__(self, path, colour, number):
+    def __init__(self, path:str, colour:str, number:int):
         super().__init__(path)
         self.colour = colour
         self.number = number
  
 class SwitchCard(Card):
-    def __init__(self, path):
+    def __init__(self, path:str):
         self.path = path
         
 
 class DiceCard(Card):
-    def __init__(self, path):
+    def __init__(self, path:str):
         super().__init__(path)
 
 

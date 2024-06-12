b629a9100da4d2ad75fd2c37bac000191543b355
SirBrahms <marc@famile-merz.ch>
2024-06-12 22:06:50 +0200

    Fix (?) LimList

--- LimList.py
+++ LimList.py
@@ -1,12 +1,21 @@
+from Card import *
+
 class LimList():
     def __init__(self, lst):
         self.inner_list = lst
     
-    def check_new_element(self, e):
-        if (e in self.inner_list):
-            raise ValueError("Same Card Added Twice")
-        else:
-            self.inner_list.append(e)
+    def check_new_element(self, to_add):
+        c_card_temp = ColourCard("aaa", "green", "1") # temporary card(s) to generate the type
+        d_card_temp = DiceCard("aaa")
+        s_card_temp = SwitchCard("aaaa")
+        for e in self.inner_list:
+            if (type(e) == c_card_temp and type(to_add) == c_card_temp):
+                if (e.colour == to_add.colour or e.number == to_add.number):
+                    raise ValueError()
+            elif (type(e) == d_card_temp and type(to_add) == d_card_temp):
+                raise ValueError()
+            elif (type(e) == s_card_temp and type(to_add) == s_card_temp):
+                raise ValueError()
     
     def append(self, e):
         self.inner_list.append(self.check_new_element(e))
